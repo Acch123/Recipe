39048f2d31de995063ae5d92b93a2c6fae804699
Acch <chinghong.chan0811@gmail.com>
2023-08-20 11:49:03 +1000

    hi

--- js/script.js
+++ js/script.js
@@ -10,6 +10,21 @@ const recipes = [
       ingredients: "Chicken, bell peppers, broccoli, soy sauce, garlic, ginger",
       instructions: "Stir-fry chicken and vegetables. Add soy sauce, garlic, and ginger. Serve over rice."
     },
+    {
+      title: "Classic Margherita Pizza",
+      ingredients: "Pizza dough, tomato sauce, mozzarella cheese, fresh basil leaves, olive oil",
+      instructions: "Roll out the dough, spread tomato sauce, add cheese, bake until golden. Top with basil and drizzle with olive oil."
+    },
+    {
+      title: "Creamy Spinach and Mushroom Pasta",
+      ingredients: "Pasta, spinach, mushrooms, heavy cream, garlic, Parmesan cheese",
+      instructions: "Cook pasta. Sauté garlic, mushrooms, and spinach. Add cream and Parmesan. Toss with cooked pasta."
+    },
+    {
+      title: "Grilled Vegetable Skewers",
+      ingredients: "Assorted vegetables (bell peppers, zucchini, cherry tomatoes, onions), olive oil, balsamic vinegar, herbs",
+      instructions: "Thread veggies onto skewers, brush with olive oil and balsamic vinegar mixture. Grill until charred and tender."
+    }
     // Add more recipe objects as needed
   ];
   
@@ -93,3 +108,20 @@ recipeForm.addEventListener("submit", function(event) {
 
 
 displayRecipes(); // Display recipes when the page loads
+
+
+// Searching
+const searchInput = document.getElementById("searchInput");
+const searchButton = document.getElementById("searchButton");
+
+searchButton.addEventListener("click", searchRecipe);
+
+function searchRecipe() {
+  const searchTerm = searchInput.value.toLowerCase();
+
+  const matchingRecipes = recipes.filter(recipe => {
+    return recipe.title.toLowerCase().includes(searchTerm);
+  });
+
+  displayRecipes(matchingRecipes);
+}
